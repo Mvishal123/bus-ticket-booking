3a670b3f9230149bf04b67d8bd619515cb9bfcab
Vishal-m <vishalmahesh.study@gmail.com>
2024-03-27 15:30:58 +0530

    ui: bus seat booked indicator

--- src/components/bus-seat.tsx
+++ src/components/bus-seat.tsx
@@ -13,7 +13,7 @@ const BusSeat = ({ data, busId }: BusSeatProps) => {
   const selected = selectedSeats.includes(data.seatNumber);
   return (
     <button
-      className={`${isBooked && "cursor-not-allowed bg-slate-100"} ${
+      className={`${isBooked && "cursor-not-allowed bg-slate-300"} ${
         selected && "border-green-500 bg-green-400/50"
       } border-2 rounded w-12 sm:w-20 py-2`}
       disabled={isBooked}
@@ -28,7 +28,7 @@ const BusSeat = ({ data, busId }: BusSeatProps) => {
       <div className="flex justify-between px ">
         <div className="flex-1 text-center font-semibold text-xs sm:text-lg rotate-90 sm:rotate-0">
           {data.seatNumber}
-          {data.type == SeatingType.LOWER ? "L" : "U"}
+          {data.type == "lower" ? "L" : "U"}
         </div>
         <div className="w-[10px]  border-2  mr-2"></div>
       </div>
